76357ad509b8a591ec7a56c83899553e41b9b1f7
EclipseGame18 <famousferguslive@gmail.com>
2023-04-20 23:04:02 +1000

    Add guild_welcome and Guild_log

---
+++ commands/guild_log_channel.js
@@ -0,0 +1,61 @@
+const { CommandType } = require("wokcommands");
+const { PermissionsBitField } = require('discord.js')
+const GuildWelcomeChannel = require('../Guild4')
+
+module.exports = {
+  // command options
+  description: "Input the channel ID ot be used as a log channel. Leave blank to disable",
+  catagory: 'Utility Commands',
+  aliases: ['log_channel'],
+  maxArgs: 30,
+  expectedArgs: "[channel ID]",
+  guildOnly: true,
+  
+  // Create a legacy and slash command
+  type: CommandType.BOTH,
+
+  // Invoked when a user runs the command
+  callback: async ({ message, client, channel, interaction, options, args, text, guild, user, member }) => {
+    const guildwelcomechannel = await GuildWelcomeChannel.findOne({_id: guild.id}).catch(error =>{
+		console.log(`There was a error`)
+	})
+    if (!member.permissions.has(PermissionsBitField.Flags.ManageGuild)) return ':x: Unable to comply, you do not have \`Manage_Guild\` permision.'
+    let newText
+    if(!guildwelcomechannel){
+        await GuildWelcome.findOneAndUpdate({
+            _id: guild.id
+            },{
+            _id: guild.id,
+            message: '',
+                
+            },{
+                upsert: true
+            })
+       }
+       if(!text){
+        newText = ''
+       } else{
+        newText = text
+       }
+       if(isNaN(newText)){
+        return 'Please specify the channel ID as a number. e.g. 1234567891112131415'
+       }
+       if(newText.length > 30){
+        return `Channel IDs usually arn't bigger than 30 characters`
+       }
+       await GuildWelcomeChannel.findOneAndUpdate({
+        _id: guild.id
+        },{
+        _id: guild.id,
+        channel: newText,
+            
+        },{
+            upsert: true
+        })
+        if(newText === ''){
+            return `Successfully toggled log channel to: \`off\``
+        } else{
+        return `Successfully toggled log channel plugin to: \`on\` and changed the server log channel to: \`${newText}\``
+        }
+  },
+}
---
+++ commands/guild_welcome.js
@@ -0,0 +1,57 @@
+const { CommandType } = require("wokcommands");
+const { PermissionsBitField } = require('discord.js')
+const GuildWelcome = require('../Guild3')
+
+module.exports = {
+  // command options
+  description: "Sets the welcome message for if a user joins your server. Leave blank to disable. Use {member} to input the users username",
+  catagory: 'Utility Commands',
+  maxArgs: 100,
+  expectedArgs: "[message]",
+  guildOnly: true,
+  
+  // Create a legacy and slash command
+  type: CommandType.BOTH,
+
+  // Invoked when a user runs the command
+  callback: async ({ message, client, channel, interaction, options, args, text, guild, user, member }) => {
+    const guildwelcome = await GuildWelcome.findOne({_id: guild.id}).catch(error =>{
+		console.log(`There was a error`)
+	})
+    if (!member.permissions.has(PermissionsBitField.Flags.ManageGuild)) return ':x: Unable to comply, you do not have \`Manage_Guild\` permision.'
+    let newText
+   if(!guildwelcome){
+    await GuildWelcome.findOneAndUpdate({
+        _id: guild.id
+        },{
+        _id: guild.id,
+        message: '',
+            
+        },{
+            upsert: true
+        })
+   }
+   if(!text){
+    newText = ''
+   } else{
+    newText = text
+   }
+   if(newText.length > 100){
+    return 'Please enter a message less that 100 characters'
+   }
+   await GuildWelcome.findOneAndUpdate({
+    _id: guild.id
+    },{
+    _id: guild.id,
+    message: newText,
+        
+    },{
+        upsert: true
+    })
+    if(newText === ''){
+        return `Successfully toggled welcome message plugin to: \`off\``
+    } else{
+    return `Successfully toggled the welcome message plugin to: \`on\` and changed the server welcome message to: \`${newText}\``
+    }
+  },
+}
