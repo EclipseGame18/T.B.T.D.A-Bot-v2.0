4cb8af873d580775dc22eee0414a7545773d955f
EclipseGame18 <famousferguslive@gmail.com>
2023-07-02 01:25:08 +1000

    fix

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ client.on('ready', async() => {
         status: 'online',
         activities: [
             {
-                name: "Version 2.0 pre 2.6",
+                name: "Version 2.0 pre 2.5",
                 type: ActivityType.Watching,
             }
         ]
